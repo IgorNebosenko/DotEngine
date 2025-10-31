2c21318631307bce86d433e28f6c40e9fbd0353d
IhorNebosenko <igor.nebosenko@gmail.com>
2025-10-31 08:13:54 +0200

    Add ToolBar w\o images

---
+++ DotEngine/DotEngineEditor.UserControls/ToolBar.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DotEngineEditor.UserControls;
+
+public partial class ToolBar : UserControl
+{
+    public ToolBar()
+    {
+        InitializeComponent();
+    }
+
+    #region Navigation buttons
+    private void LookOutclick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void MoveObjectClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RotateObjectClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ScaleObjectClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+    #region Help buttons
+    private void HelpClicked(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+    #region Play buttons
+    private void PlayClicked(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void PauseClicked(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StepClicked(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+}
