904aae518914f0fe31a671f2ad92b6127eb01947
Ihor Nebosenko <igor.nebosenko@gmail.com>
2025-11-03 19:13:55 +0200

    Add EngineData

---
+++ DotEngine/Kernel/Engine/EngineData.cs
@@ -0,0 +1,18 @@
+﻿namespace Kernel.Engine;
+
+[Serializable]
+public class EngineVersion(int TopVersion, int MiddleVersion, int LowVersion)
+{
+    public int TopVersion;
+    public int MiddleVersion;
+    public int LowVersion;
+
+    public static EngineVersion CurrentVersion => new EngineVersion(0, 0, 1);
+}
+
+[Serializable]
+public class EngineData
+{
+    public string LastProjectPath;
+    public EngineVersion EngineVersion;
+}
