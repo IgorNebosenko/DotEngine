eeb392d3e3530c557a70daa1d7669ffe54d62478
Ihor Nebosenko <igor.nebosenko@gmail.com>
2025-11-03 19:38:17 +0200

    Engine data holder

---
+++ DotEngine/Kernel/Engine/EngineMetaDataHolder.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Kernel.Engine;
+
+public class EngineMetaDataHolder
+{
+    private static readonly string MetaDataPath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "DotEngine");
+    private static readonly string EngineDataPath = Path.Combine(MetaDataPath, "EngineData.json");
+    
+    public EngineData? Data { get; private set; }
+    
+    public bool TryReadMetaData()
+    {
+        if (!Directory.Exists(MetaDataPath) || !File.Exists(EngineDataPath))
+            return false;
+
+        var json = File.ReadAllText(EngineDataPath);
+        Data = JsonConvert.DeserializeObject<EngineData>(json);
+
+        return true;
+    }
+}
