f1d82ca41e18828160d40a36a64dd90be37dca15
Ihor Nebosenko <igor.nebosenko@gmail.com>
2025-11-06 19:25:01 +0200

    Add instantiate of different panels

--- DotEngine/DotEngineEditor/MainWindowParts/MainWindowWindow.cs
+++ DotEngine/DotEngineEditor/MainWindowParts/MainWindowWindow.cs
@@ -1,7 +1,10 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using AvalonDock.Layout;
 using DotEngineEditor.Themes;
 using DotEngineEditor.UserControls;
 using DotEngineEditor.UserControls.Interfaces;
+using SharpDX;
 
 namespace DotEngineEditor;
 
@@ -17,25 +20,54 @@ public partial class MainWindow : IWindowTabHandler
 
     public void InstantiateHierarchy()
     {
+        InstantiateUserComponent<HierarchyPanel>("Hierarchy", new Vector2(400f, 600f));
     }
 
     public void InstantiateProject()
     {
+        InstantiateUserComponent<ProjectPanel>("Project", new Vector2(800f, 400f));
     }
 
     public void InstantiateConsole()
     {
+        InstantiateUserComponent<ConsolePanel>("Console", new Vector2(800f, 400f));
     }
 
     public void InstantiateInspector()
     {
+        InstantiateUserComponent<InspectorPanel>("Inspector", new Vector2(400f, 600f));
     }
 
     public void InstantiateSceneView()
     {
+        //ToDo write this!
+        //InstantiateUserComponent<SceneViewPanel>("SceneView", new Vector2(600f, 800f));
     }
 
     public void InstantiateGame()
     {
+        //ToDo write this!
+        //InstantiateUserComponent<GamePanel>("Game", new Vector2(600f, 800f));
+    }
+
+    private void InstantiateUserComponent<T>(string title, Vector2 size) where T : UserControl, new()
+    {
+        var instance = new T();
+        
+        var anchorable = new LayoutAnchorable
+        {
+            Title = title,
+            Content = instance,
+            CanClose = true,
+            CanFloat = true,
+            FloatingWidth = size.X,
+            FloatingHeight = size.Y
+        };
+        
+        var anchorablePane = new LayoutAnchorablePane(anchorable);
+        
+        DockManager.Layout.RootPanel.Children.Add(anchorablePane);
+        
+        anchorable.Float();
     }
 }
