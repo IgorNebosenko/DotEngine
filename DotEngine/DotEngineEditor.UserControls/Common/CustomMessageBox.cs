af1e73a87cd54b13313ac930cbd2a77f59ed4ebf
Ihor Nebosenko <igor.nebosenko@gmail.com>
2025-11-07 21:01:12 +0200

    CustomMessageBox class

---
+++ DotEngine/DotEngineEditor.UserControls/Common/CustomMessageBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DotEngineEditor.UserControls.Common;
+
+public static class CustomMessageBox
+{
+    public static void NotImplement(string message)
+    {
+        MessageBox.Show(message, "Not Implement!", 
+            MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+}
