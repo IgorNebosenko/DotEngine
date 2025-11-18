20b88c4433aea194fe973c260beb3d1f8eacea56
IhorNebosenko <igor.nebosenko@gmail.com>
2025-11-18 20:18:17 +0200

    Update MeshRenderer

--- DotEngine/DotEngine/Components/Mesh/MeshRenderer.cs
+++ DotEngine/DotEngine/Components/Mesh/MeshRenderer.cs
@@ -1,6 +1,12 @@
-﻿namespace DotEngine;
+﻿using Attributes;
+using DxStructures;
+
+namespace DotEngine;
 
 public class MeshRenderer : Component
 {
+    [ShowInInspector] private Shader[] meshShaders;
+    [ShowInInspector] private bool castShadows;
+    
     public string Name => "MeshRenderer";
 }
